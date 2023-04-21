e6706eff2b8f3dc5cb2fe8a93d99b3d8d877bc8d
kaouthar-elyou <kaoutharelyoubi1997@gmail.com>
2023-04-20 20:01:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - A  program that prints a new line with puts function 
+ * Return: 0 (success)
+ */
 int main(void)
 {
-printf("\"Programming is like building a multilingual puzzle");
-return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 } 
