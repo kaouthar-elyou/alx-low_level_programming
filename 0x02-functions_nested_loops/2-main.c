5d1f53076b3a80fb3b2f5e0c0497695bd9d0bad2
kaouthar-elyou <kaoutharelyoubi1997@gmail.com>
2023-04-26 03:08:37 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,15 +1,13 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-
 int main(void)
+
 {
 	print_alphabet_x10();
 	return (0);
-
 }
-
