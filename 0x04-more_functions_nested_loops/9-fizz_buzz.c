733e97a08c2ad798a886e30054029bed5afe0c04
kaouthar-elyou <kaoutharelyoubi1997@gmail.com>
2023-04-27 06:36:31 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,8 +10,8 @@
 
 int main(void)
 {
-int i;
 
+int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
@@ -23,7 +23,7 @@ else if (i % 5 == 0 && i % 3 != 0)
 printf(" Buzz");
 }
 else if (i % 3 == 0 && i % 5 == 0)
- {
+{
 printf(" FizzBuzz");
 }
 else if (i == 1)
@@ -36,6 +36,5 @@ printf(" %d", i);
 }
 }
 printf("\n");
-
 return (0);
 }
