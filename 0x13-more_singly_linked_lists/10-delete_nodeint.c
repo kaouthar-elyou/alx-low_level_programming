191945ada1a15436d1d06675f1616b20eef12a4a
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-06-06 15:08:48 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - function  deletes a node at index of a linked list.
+ * delete_nodeint_at_index - function  deletes a node at index of a list.
  * @head: pointer to the first element in the list
  * @index: index of the node to delete
  *
@@ -9,29 +9,28 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *temp = *head;
-listint_t *current = NULL;
-unsigned int i = 0;
+	listint_t *temp = *head;
+	listint_t *current = NULL;
 
-if (*head == NULL)
-return (-1);
-if (index == 0)
-{
-*head = (*head)->next;
-free(temp);
-return (1);
-}
-while (i < index - 1)
-{
-if (!temp || !(temp->next))
-return (-1);
-temp = temp->next;
-i++;
-}
+	unsigned int i = 0;
 
-current = temp->next;
-temp->next = current->next;
-free(current);
-
-return (1);
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+	while (i < index - 1)
+	{
+		if (!temp || !(temp->next))
+			return (-1);
+		temp = temp->next;
+		i++;
+	}
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
+	return (1);
 }
--- 0x13-more_singly_linked_lists/10-main.c
+++ 0x13-more_singly_linked_lists/10-main.c
@@ -13,7 +13,6 @@ int main(void)
 	listint_t *head;
 
 	head = NULL;
-
 add_nodeint_end(&head, 0);
 add_nodeint_end(&head, 1);
 add_nodeint_end(&head, 2);
@@ -23,47 +22,60 @@ add_nodeint_end(&head, 98);
 add_nodeint_end(&head, 402);
 add_nodeint_end(&head, 1024);
 print_listint(head);
-printf("-----------------\n");
+printf("--\n");
 delete_nodeint_at_index(&head, 5);
 print_listint(head);
-printf("-----------------\n");
+printf("--\n");
 delete_nodeint_at_index(&head, 0);
 print_listint(head);
-printf("-----------------\n");
+printf("--\n");
 delete_nodeint_at_index(&head, 0);
 print_listint(head);
-printf("-----------------\n");
+printf("--\n");
 delete_nodeint_at_index(&head, 0);
 print_listint(head);
-printf("-----------------\n");
+printf("--\n");
 delete_nodeint_at_index(&head, 0);
 print_listint(head);
-printf("-----------------\n");
+printf("--\n");
 delete_nodeint_at_index(&head, 0);
 print_listint(head);
-printf("-----------------\n");
+printf("--\n");
 delete_nodeint_at_index(&head, 0);
+
 print_listint(head);
-printf("-----------------\n");
+printf("--\n");
+
 delete_nodeint_at_index(&head, 0);
-printf("-----------------\n");
+printf("--\n");
+
 delete_nodeint_at_index(&head, 0);
-printf("-----------------\n");
+printf("--\n");
+
 delete_nodeint_at_index(&head, 0);
-printf("-----------------\n");
+printf("--\n");
+
 delete_nodeint_at_index(&head, 0);
-printf("-----------------\n");
+printf("--\n");
+
 delete_nodeint_at_index(&head, 0);
-printf("-----------------\n");
+printf("--\n");
+
 delete_nodeint_at_index(&head, 0);
-printf("-----------------\n");
+printf("--\n");
+
 delete_nodeint_at_index(&head, 0);
-printf("-----------------\n");
+printf("--\n");
+
 delete_nodeint_at_index(&head, 0);
-printf("-----------------\n");
+printf("--\n");
+
 delete_nodeint_at_index(&head, 0);
-printf("-----------------\n");
+printf("--\n");
+
 delete_nodeint_at_index(&head, 0);
+
 print_listint(head);
+
 return (0);
 }
