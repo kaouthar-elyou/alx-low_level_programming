297342ef0ae484078a3ad8b828444a5e764062d5
kaouthar-elyou <kaoutharelyoubi1997@gmail.com>
2023-06-08 15:20:01 -0700

    0x14 bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit_vl;
 
-	if (index > 0)
+	if (index > 63)
 		return (-1);
 
 	bit_vl = (n >> index) & 1;
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,7 +8,7 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 0)
+	if (index > 63)
 		return (-1);
 
 	*n = (~(1UL << index) & *n);
